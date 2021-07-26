7f09725dea4e49378ba79073f0ac3eff7d60165f
jiwon79 <19-079@ksa.hs.kr>
2021-07-26 12:29:02 +0900

    feat: Add Header html

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <p className="logo">LOGO</p>
+      <li className="gnb">
+        <ul>스페이스 저니</ul>
+        <ul>마이스페이스</ul>
+        <ul>아워스페이스</ul>
+      </li>
+    </div>
+  )
+}
