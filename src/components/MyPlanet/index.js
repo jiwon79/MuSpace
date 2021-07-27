c14b1dd87637fd6bd0703939cce6da8cf03a1dd2
jiwon79 <19-079@ksa.hs.kr>
2021-07-28 01:15:25 +0900

    feat: Add change planet size animation

--- src/components/MyPlanet/index.js
+++ src/components/MyPlanet/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Planet.css'
 
-export default function MyPlanet() {
+export default function MyPlanet({focus}) {
   return (
     <div className="planet">
+      <img 
+        src="/img/circle.png" 
+        alt="planet"
+        className={['planet__img', focus && 'planet-animation'].join(' ')}
+      />
     </div>
   )
 }
--- src/pages/MySpace.js
+++ src/pages/MySpace.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './../components/Header'
 import MyPlanet from './../components/MyPlanet'
 
 export default function MySpace() {
+  const [focus, setFocus] = useState(false);
+
   return (
     <div>
       <Header/>
-      <MyPlanet/>
+      <button onClick={() => {
+        console.log(focus);
+        setFocus(!focus);
+        }
+      }>animation</button>
+      <MyPlanet
+        focus = {focus}
+      />
     </div>
   )
 }
