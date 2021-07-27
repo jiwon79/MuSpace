f82e81e78c26c70143d33653c99fc8e2d626344a
jiwon79 <19-079@ksa.hs.kr>
2021-07-28 00:17:13 +0900

    feat: Add Planet component

---
+++ src/components/MyPlanet/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Planet.css'
+
+export default function MyPlanet() {
+  return (
+    <div className="planet">
+    </div>
+  )
+}
--- src/pages/MySpace.js
+++ src/pages/MySpace.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './../components/Header'
+import MyPlanet from './../components/MyPlanet'
 
 export default function MySpace() {
   return (
     <div>
       <Header/>
-      My Sapce
+      <MyPlanet/>
     </div>
   )
 }
