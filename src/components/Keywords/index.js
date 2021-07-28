5869a6b072d4d6e1dc8ecac2beb9aebf5cf3ce94
jiwon79 <19-079@ksa.hs.kr>
2021-07-28 17:32:10 +0900

    feat: Add keyword css on planet page

--- src/components/Keywords/index.js
+++ src/components/Keywords/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Keywords.css'
+
+const KeywordItem = ({keyword}) => {
+  return (
+    <div className="keywords__item">
+      {keyword}
+    </div>
+  )
+}
 
 export default function Keywords() {
+  var keywordList = ["취미", "음식", "수집", "여행", "음악", "운동", "영화"];
+
   return (
-    <div>
-      키워드
+    <div className="keywords">
+      {keywordList.map(
+        (keyword) => <KeywordItem keyword={keyword}/>
+      )
+      }
     </div>
   )
 }
