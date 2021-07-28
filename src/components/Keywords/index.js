2e1ce8a8edcdc72dde6a3a8ee6ec68284fd89ebd
jiwon79 <19-079@ksa.hs.kr>
2021-07-28 16:54:02 +0900

    feat: Add message on Planet page

---
+++ src/components/Keywords/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Keywords() {
+  return (
+    <div>
+      키워드
+    </div>
+  )
+}
--- src/pages/MySpace/Planet.js
+++ src/pages/MySpace/Planet.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import BackgroundPlanet from './../../components/BackgroundPlanet'
+import Keywords from './../../components/Keywords'
+import './Planet.css'
 
 export default function Planet({menu, setMenu}) {
   useEffect(() => {
@@ -8,10 +10,13 @@ export default function Planet({menu, setMenu}) {
   
   return (
     <div>
-      planet
       <BackgroundPlanet
         focus={true}
       />
+      <div className="message">
+        옷을 희망의 주는 구형 말이다. 찾아 보이는 청춘을 교향악이다. 반짝이는 가치를 거선의 아니다.
+      </div>
+      <Keywords/>
     </div>
   )
 }
